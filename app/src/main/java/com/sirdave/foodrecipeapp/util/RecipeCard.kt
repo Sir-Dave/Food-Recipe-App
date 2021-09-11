2aba3c4af7a33e5f7da8a0a963e05146de2e82db
Abiola David <davidolanrewaju2000@gmail.com>
2021-09-11 16:23:33 +0100

    Loaded image asynchronously into view

--- app/src/main/java/com/sirdave/foodrecipeapp/util/RecipeCard.kt
+++ app/src/main/java/com/sirdave/foodrecipeapp/util/RecipeCard.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -16,42 +18,47 @@ import com.sirdave.foodrecipeapp.R
 import com.sirdave.foodrecipeapp.domain.model.Recipe
 
 @Composable
-fun RecipeCard(recipe: Recipe, onClick: () -> Unit){
-    Card(modifier = Modifier.padding(top = 6.dp, bottom = 6.dp)
+fun RecipeCard(recipe: Recipe, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier.padding(top = 6.dp, bottom = 6.dp)
             .fillMaxWidth()
             .clickable(onClick = onClick),
-            elevation = 8.dp
-    ){
-        Column{
-            recipe.sourceUrl?.let {
-                Image(painter = painterResource(id = R.drawable.empty_plate),
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxWidth().height(225.dp))
+        elevation = 8.dp
+    ) {
+        Column {
+            recipe.featuredImage?.let { url ->
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth().height(225.dp)
+                    )
+                }
+            }
+            recipe.title?.let { title ->
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(
+                        top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp
+                    )
+                ) {
+                    Text(
+                        text = title,
+                        modifier = Modifier.fillMaxWidth(0.85f)
+                            .wrapContentWidth(Alignment.Start),
+                        style = MaterialTheme.typography.h5
+                    )
 
-                recipe.title?.let {title->
-                    Row(modifier = Modifier.fillMaxWidth().padding(
-                            top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp)
-                    ){
-                        Text(
-                                text = title,
-                                modifier = Modifier.fillMaxWidth(0.85f)
-                                        .wrapContentWidth(Alignment.Start),
-                                style = MaterialTheme.typography.h5
-                        )
-
-                        Text(
-                                text = recipe.rating.toString(),
-                                modifier = Modifier.fillMaxWidth()
-                                        .wrapContentWidth(Alignment.End)
-                                        .align(Alignment.CenterVertically),
-                                style = MaterialTheme.typography.h6
-                        )
-
-                    }
+                    Text(
+                        text = recipe.rating.toString(),
+                        modifier = Modifier.fillMaxWidth()
+                            .wrapContentWidth(Alignment.End)
+                            .align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.h6
+                    )
                 }
             }
         }
     }
-
 }
