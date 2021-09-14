39a5191755d9b2f3952323f79d1ea372516eeed6
Abiola David <davidolanrewaju2000@gmail.com>
2021-09-14 15:50:29 +0100

    Added chips to display categories

--- app/src/main/java/com/sirdave/foodrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/sirdave/foodrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,15 +18,16 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.sirdave.foodrecipeapp.presentation.components.FoodCategoryChip
 import com.sirdave.foodrecipeapp.util.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -40,7 +45,7 @@ class RecipeListFragment : Fragment() {
                 Column{
                     Surface(modifier = Modifier.fillMaxWidth(),
                         elevation = 8.dp,
-                        color = MaterialTheme.colors.primary){
+                        color = Color.White){
                         Column {
                             Row(modifier = Modifier.fillMaxWidth()) {
                                 TextField(value = query, onValueChange = {newValue ->
@@ -69,14 +74,12 @@ class RecipeListFragment : Fragment() {
                             }
 
                             LazyRow(modifier = Modifier.fillMaxWidth()){
-                                for (category in getAllFoodCategories()){
-                                    /**Text(text = category.value,
-                                        style = MaterialTheme.typography.body2,
-                                        color = MaterialTheme.colors.secondary,
-                                        modifier = Modifier.padding(8.dp)
-                                    )*/
+                                items(getAllFoodCategories()) {
+                                    FoodCategoryChip(category = it.value, onExecuteSearch = { s ->
+                                        viewModel.onQueryChanged(s)
+                                        viewModel.newSearch(s)
+                                    } )
                                 }
-
                             }
                         }
                     }
