920e43b016118b5326c8463fb44b37ad519b78d4
Abiola David <davidolanrewaju2000@gmail.com>
2021-10-30 13:07:39 +0100

    SnackbarState and SnackbarHostState

--- app/src/main/java/com/sirdave/foodrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/sirdave/foodrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,18 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.sirdave.foodrecipeapp.presentation.BaseApplication
 import com.sirdave.foodrecipeapp.presentation.components.CircularIndeterminateProgressBar
 import com.sirdave.foodrecipeapp.presentation.components.LoadingRecipeListShimmer
@@ -24,6 +31,7 @@ import com.sirdave.foodrecipeapp.presentation.components.SearchAppBar
 import com.sirdave.foodrecipeapp.ui.FoodRecipeAppTheme
 import com.sirdave.foodrecipeapp.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -38,7 +46,27 @@ class RecipeListFragment : Fragment() {
                               savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                FoodRecipeAppTheme(darkTheme = application.isDark.value) {
+                //val isShowing = remember{ mutableStateOf(false)}
+                val snackbarHostState = remember{SnackbarHostState()}
+                Column{
+                    Button(onClick = {
+                        lifecycleScope.launch {
+                            snackbarHostState.showSnackbar(
+                                message = "Hey look a snackbar",
+                                actionLabel = "Hide",
+                                duration = SnackbarDuration.Short
+                            )
+                        }
+                    }) {
+                        Text(text = "Show snackbar")
+                    }
+                    decoupledSnackBar(snackbarHostState = snackbarHostState)
+                    /**SnackBarDemo(isShowing = isShowing.value, onHideSnackBar = {
+                        isShowing.value = false
+                    })*/
+                }
+
+                 /** FoodRecipeAppTheme(darkTheme = application.isDark.value) {
 
                     val recipes = viewModel.recipes.value
                     val query = viewModel.query.value
@@ -75,8 +103,50 @@ class RecipeListFragment : Fragment() {
                             CircularIndeterminateProgressBar(isDisplayed = loading)
                         }
                     }
-                }
+                }*/
             }
         }
+    }
+}
+
+
+@Composable
+fun decoupledSnackBar(
+    snackbarHostState: SnackbarHostState){
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val snackbar = createRef()
+        SnackbarHost(
+            modifier = Modifier.constrainAs(snackbar) {
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            hostState = snackbarHostState,
+            snackbar = {
+                Snackbar(
+                    action = {
+                        TextButton(
+                            onClick = {snackbarHostState.currentSnackbarData?.dismiss()}
+                        ) {
+                            Text(text = snackbarHostState.currentSnackbarData?.actionLabel?: "",
+                                style = TextStyle(color = Color.White)) }
+                    }
+                ) {
+                    Text(text = snackbarHostState.currentSnackbarData?.message?: "")
+                }
+            }
+        )
+    }
+
+}
+
+
+
+@Composable
+fun SnackBarDemo(
+    isShowing: Boolean,
+    onHideSnackBar: () -> Unit
+){
+    if (isShowing){
     }
 }
